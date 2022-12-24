b8cbba5e2f8b5173001b57901a7b0a0905c99a40
Vinay Patil <vinay1645.vp@gmail.com>
2022-12-24 19:29:37 +0530

    data fetching

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useState ,useEffect} from "react";
 import './App.css';
-
+import Table from './components/table';
 function App() {
   const [startDateValue, setstartDateValue] = useState(new Date());
   const [endDateValue, setendDateValue] = useState(null);
+  var columnNames=["Date","app_id","requests","responses","impressions","clicks","revenue"]
+  const [data,setData]=useState("");
 
+  useEffect(() => {
+    fetch(`http://go-dev.greedygame.com/v3/dummy/report?startDate=${startDateValue}&endDate=${endDateValue}`)
+      .then(response => response.json())
+      .then(json => setData(json))
+      
+  }, [startDateValue,endDateValue]);
+  
   function handleStartDateUpdate(e) {
     const dateValue=e.target.value;
     console.log(dateValue);
     setstartDateValue(dateValue);
-    console.log(startDateValue);
+    console.log(data);
   }
   
   function handleEndDateUpdate(e) {
@@ -17,13 +26,19 @@ function App() {
     setendDateValue(dateValue);
     console.log(endDateValue);
   }
+
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Analytics</h1>
+      <h2>Analytics</h2>
       <input type="date" onChange={(e) => handleStartDateUpdate(e)} id="startDate" name="startDate"></input>
       <input type="date" id="endDate" name="endDate"onChange={(e) => handleEndDateUpdate(e)}></input>
-     
+     <button name="settings">Settings</button>
+      <div className="Metrics">
+        <h3>Dimensions and Metrics</h3>
+     {  data && <Table tabledata={data} tableColumns={columnNames}/>        
+}
+</div>
       </header>
     </div>
   );
---
+++ src/components/table.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Table(tabledata,tableColumns) {
+
+  return (
+    
+
+    <div>table</div>
+  
+    )
+}
+
+export default Table
